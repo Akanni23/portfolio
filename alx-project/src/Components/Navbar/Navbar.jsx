2561488d66e0bdeff3c7e49428a87708835250b9
Akanni23 <muhasy2k@yahoo.com>
2025-01-17 14:31:19 -0800

    don

---
+++ alx-project/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react"; r
+import "./Navbar.css"
+import logo from '../Assets/EGC-preview.png'
+import cart_icon from '../Assets/EGC-preview.png'
+const Navbar = () => {
+    return (
+        <div className="navbar">
+
+        </div>
+    )
+}
+
+export default Navbar
