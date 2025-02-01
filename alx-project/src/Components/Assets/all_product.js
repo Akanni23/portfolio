35c3d918128c18d07f25f9e18f1317093abb01e2
Akanni23 <muhasy2k@yahoo.com>
2025-01-31 19:12:25 -0800

    done

--- alx-project/src/Components/Assets/all_product.js
+++ alx-project/src/Components/Assets/all_product.js
@@ -1,1 +1,247 @@
-import { bunner } from "./data";
+import p1_img from "./product_01.png";
+import p2_img from "./product_02.png";
+import p3_img from "./product_03.png";
+import p4_img from "./product_04.png";
+import p6_img from "./product_05.png";
+import p7_img from "./product_07.png";
+import p9_img from "./product_09.png";
+import p10_img from "./product_010.png";
+import p11_img from "./product_011.png";
+import p12_img from "./product_012.png";
+import p13_img from "./product_013.png";
+import p14_img from "./product_014.png";
+import p16_img from "./product_016.png";
+import p18_img from "./product_018.png";
+import p19_img from "./product_019.png";
+import p21_img from "./product_021.png";
+import p23_img from "./product_023.png";
+import p24_img from "./product_024.png";
+import p25_img from "./product_025.png";
+import p26_img from "./product_026.png";
+import p27_img from "./product_027.png";
+import p28_img from "./product_028.png";
+import p30_img from "./product_030.png";
+import p32_img from "./product_032.png";
+import p33_img from "./product_033.png";
+import p34_img from "./product_034.png";
+import p35_img from "./product_035.png";
+
+let all_product = [
+    {
+        id: 1,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p1_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 2,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p2_img,
+        new_price: 25000,
+        old_price: 24500,
+    },
+    {
+        id: 3,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p3_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 4,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p4_img,
+        new_price: 25000,
+        old_price: 24500,
+    },
+    {
+        id: 6,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p6_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 7,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p7_img,
+        new_price: 25000,
+        old_price: 24500,
+    },
+    {
+        id: 9,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p9_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 10,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p10_img,
+        new_price: 25000,
+        old_price: 24500,
+    },
+    {
+        id: 11,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p11_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 12,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p12_img,
+        new_price: 25000,
+        old_price: 24500,
+    },
+    {
+        id: 13,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p13_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 14,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p14_img,
+        new_price: 25000,
+        old_price: 24500,
+    },
+    {
+        id: 16,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p16_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 18,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p18_img,
+        new_price: 25000,
+        old_price: 24500,
+    },
+    {
+        id: 19,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p19_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 21,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p21_img,
+        new_price: 25000,
+        old_price: 24500,
+    },{
+        id: 23,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p23_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 24,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p24_img,
+        new_price: 25000,
+        old_price: 24500,
+    },
+    {
+        id: 25,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p25_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 26,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p26_img,
+        new_price: 25000,
+        old_price: 24500,
+    },
+    {
+        id: 27,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p27_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 28,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p28_img,
+        new_price: 25000,
+        old_price: 24500,
+    },
+    {
+        id: 30,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p30_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 42,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p32_img,
+        new_price: 25000,
+        old_price: 24500,
+    },
+    {
+        id: 33,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p33_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+    {
+        id: 34,
+        name: "3kg complete cylinder",
+        category: "Cylinder",
+        Image: p34_img,
+        new_price: 25000,
+        old_price: 24500,
+    },
+    {
+        id: 35,
+        name: "12.5 plastic cylinder",
+        category: "Cylinder",
+        Image: p35_img,
+        new_price: 42500,
+        old_price: 42000,
+    },
+
+];
+
--- alx-project/src/Components/Navbar/Navbar.jsx
+++ alx-project/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from "react"; r
+import React from "react";
 import "./Navbar.css"
-import logo from '../Assets/EGC-preview.png'
-import cart_icon from '../Assets/EGC-preview.png'
+
+import logo from "../Assets/logo.png"
+import cart_icon from "../Assets/cart_icon.png"
+import react from "react"; react
 const Navbar = () => {
     return (
-        <div className="navbar">
+        <div className='navbar'>
+            <div className="nav-logo">
+                <img src={logo} alt="" />
+                <p>2-Good Gas</p>
+            </div>
+            <ul className="nav-menu">
+                <li>Cylinder</li>
+                <li>Accessaries</li>
+                <li>Services</li>
+                <li>Marketing</li>
+            </ul>
 
         </div>
     )
